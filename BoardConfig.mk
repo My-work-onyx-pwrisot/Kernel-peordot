7970fe145b8257c2989e6861c7b9411d2ea1181d
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-31 01:40:39 +0530

    onyx: sepolicy: Allow parts to use displayfeature
    Change-Id: I7b8a8ed50da73baebe1561df6dddd00ddc3f4044

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,7 @@ include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Vendor security patch
